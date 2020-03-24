a92df8b6f790fd549f96fb9f8eabf69ed777a4cc
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-03-24 23:23:35 +0400

    changes on auth)

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User,Student,StudentInterests
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
 
 class TeacherSignUpForm(UserCreationForm):
+    email = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    username = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}))
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username','email',)
@@ -15,11 +21,15 @@ class TeacherSignUpForm(UserCreationForm):
         return user
 
 class StudentSignUpForm(UserCreationForm):
-    interests = forms.ModelMultipleChoiceField(
-        queryset=StudentInterests.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=True
-    )
+    email = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    username = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}))
+    # interests = forms.ModelMultipleChoiceField(
+    #     queryset=StudentInterests.objects.all(),
+    #     widget=forms.CheckboxSelectMultiple,
+    #     required=True
+    # )
 
     class Meta(UserCreationForm.Meta):
         model = User
@@ -43,3 +53,7 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('email',)
+
+class CustomAuthForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput(attrs={'class':'validate','placeholder': 'Email'}))
+    password = forms.CharField(widget=PasswordInput(attrs={'placeholder':'Password'}))
--- users/views.py
+++ users/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render,redirect
 from django.urls import  reverse_lazy
 from django.views import generic
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate
 from .models import User
 from django.views.generic import CreateView,TemplateView
 from .forms import StudentSignUpForm,TeacherSignUpForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
 class SignUpView(TemplateView):
-    template_name = 'users/registration/signup.html'
+    template_name = 'registration/signup.html'
 
 class StudentSignUpView(CreateView):
     model = User
     form_class = StudentSignUpForm
-    template_name = 'users/registration/signup_form.html'
+    template_name = 'registration/signup_form.html'
 
     def get_context_data(self, **kwargs):
         kwargs['user_type'] = 'student'
@@ -22,13 +23,13 @@ class StudentSignUpView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
-        login(self.request, user)
+        auth_login(self.request,user,backend='django.contrib.auth.backends.ModelBackend')
         return redirect('core:shome')
 
 class TeacherSignUpView(CreateView):
     model = User
     form_class = TeacherSignUpForm
-    template_name = 'users/registration/signup_form.html'
+    template_name = 'registration/signup_form.html'
 
     def get_context_data(self, **kwargs):
         kwargs['user_type'] = 'teacher'
@@ -36,23 +37,6 @@ class TeacherSignUpView(CreateView):
 
     def form_valid(self, form):
         user = form.save()
-        login(self.request, user)
+        auth_login(self.request,user,backend='django.contrib.auth.backends.ModelBacked')
         return redirect('core:thome')
-
-def login(request):
-    if (request.method == 'POST'):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            type_obj = user_type.objects.get(user=user)
-            if user.is_authenticated and type_obj.is_student:
-                return redirect('') #Go to student home
-            elif user.is_authenticated and type_obj.is_teach:
-                return redirect('') #Go to teacher home
-        else:
-            # Invalid email or password. Handle as you wish
-            return redirect('home')
-
-    return render(request, 'users/registration/login.html')
+        
