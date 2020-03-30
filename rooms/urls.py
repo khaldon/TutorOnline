83315cc0d474c37b82f6d248ed1ac302a661d6f4
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-03-30 14:06:27 +0400

    changes

--- rooms/urls.py
+++ rooms/urls.py
@@ -5,8 +5,8 @@ app_name = 'rooms'
 
 urlpatterns = [
     path('rooms/',RoomsView.as_view(),name='rooms'),
-    path('rooms/<room>/',join_room,name='join_room'),
     path('rooms/create_room/',create_room,name='create_room'),
+    path('rooms/<room>/',join_room,name='join_room'),
     path('rooms/<room>/invite?key=<invite_url>/',join_to_room,name='join_to_room')
 
 ]
